515e923c9b9fbfc46830e5abcfc6deb3bdcc553e
NS NooR <non377938@gmail.com>
2024-07-09 22:00:05 +0600

    dart2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,5 @@
 void main(){
-  // var number =9;
-  // int count = 0;
-  // for (int i = 2; i<number; i++){
-  //     if(number%i==0) {
-  //       count++;
-  //       break;
-  //     }
-  //   }
-  //   if(count==0){
-  //     print("prime");
-  //   }
-  //   else{
-  //     print("not prrime");
-  //   }
 
-  for(int i=1; i<=100; i++){
-    int count = 0;
-    for(int j=2; j<i; j++){
-      if(i%j==0){
-        count++;
-        break;
-      }
-    }
-    if(count==0){
-      print(i);
-    }
-  }
+
+
 }
--- lib/note.dart
+++ lib/note.dart
@@ -10,4 +10,33 @@ void main() {
 
   print(map.keys.toList());
 
+  var number =9;
+  int count = 0;
+  for (int i = 2; i<number; i++){
+    if(number%i==0) {
+      count++;
+      break;
+    }
+  }
+  if(count==0){
+    print("prime");
+  }
+  else{
+    print("not prrime");
+  }
+
+  for(int i=1; i<=100; i++){
+    int count = 0;
+    for(int j=2; j<i; j++){
+      if(i%j==0){
+        count++;
+        break;
+      }
+    }
+    if(count==0){
+      print(i);
+    }
+  }
+
+
 }
