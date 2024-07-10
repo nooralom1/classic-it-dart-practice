bd28230a1acfdbc759f4badff7a4b142e56245e4
NS NooR <non377938@gmail.com>
2024-07-10 11:09:45 +0600

    marks calculate

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
-void main(){
+void main() {
+  int marks = 60;
 
-var birthyear = 2000;
-var presentyear = DateTime.now().year-birthyear;
-
-print ("Your age is = $presentyear");
-
-
+  if(marks>=80&&marks<=100){
+    print("result is A+");
+  }
+  else  if(marks>=70&&marks<=79){
+    print("result is A");
+  }
+  else  if(marks>=60&&marks<=69){
+    print("result is A-");
+  }
+  else  if(marks>=50&&marks<=59){
+    print("result is B+");
+  }
+  else  if(marks>=40&&marks<=49){
+    print("result is B");
+  }
+  else  if(marks>=33&&marks<=39){
+    print("result is C");
+  }
+  else{
+    print("F");
+  }
 }
--- lib/note.dart
+++ lib/note.dart
@@ -10,6 +10,7 @@ void main() {
 
   print(map.keys.toList());
 
+  //prime or not prime
   var number =9;
   int count = 0;
   for (int i = 2; i<number; i++){
@@ -25,6 +26,7 @@ void main() {
     print("not prrime");
   }
 
+  //prime or not prime in a range
   for(int i=1; i<=100; i++){
     int count = 0;
     for(int j=2; j<i; j++){
@@ -38,5 +40,36 @@ void main() {
     }
   }
 
+  //age calculate
+  var birthyear = 2000;
+  var presentyear = DateTime.now().year-birthyear;
+
+  print ("Your age is = $presentyear");
+
+
+//marks calculate
+  int marks = 60;
+
+  if(marks>=80&&marks<=100){
+    print("result is A+");
+  }
+  else  if(marks>=70&&marks<=79){
+    print("result is A");
+  }
+  else  if(marks>=60&&marks<=69){
+    print("result is A-");
+  }
+  else  if(marks>=50&&marks<=59){
+    print("result is B+");
+  }
+  else  if(marks>=40&&marks<=49){
+    print("result is B");
+  }
+  else  if(marks>=33&&marks<=39){
+    print("result is C");
+  }
+  else{
+    print("F");
+  }
 
 }
