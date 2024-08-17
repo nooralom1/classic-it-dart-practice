401e2cd5132c2b36a3545b50d5f58ed34393f2b0
NS NooR <non377938@gmail.com>
2024-08-17 23:06:59 +0600

    Nested For Loop

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'dart:developer';
-import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
 
 
 void main() {
 
-  //Do While Loop
-  int n = 1;
-  do{
-    if(n%2==0){
-      log("$n");
+  //Break Keyword
+  for(int i=1; i<=10; i++){
+  log("$i ");
+  if( i==6){
+  break ;
+  }
+  }
+  //Using labels
+  myOuterLoop: for(int i= 1; i<=3; i++ ){
+    for(int j=1; j<=3; j++){
+      log("$i  $j");
+      if( i==2 && j==2 ){
+        break myOuterLoop;
+      }
     }
-    n++;
-  }while(n<=10);
+  }
+  //Nested For Loop
+  for(int i= 1; i<=10; i++ ){
+    for(int j=1; j<=i; j++){
+      log("$i");
+    }
+  }
 }
--- lib/note.dart
+++ lib/note.dart
@@ -160,4 +160,29 @@ void main() {
     n++;
   }while(n<=10);
 
+
+  //Break Keyword
+  for(int i=1; i<=10; i++){
+    log("$i ");
+    if( i==6){
+      break ;
+    }
+  }
+  //Using labels
+  myOuterLoop: for(int i= 1; i<=3; i++ ){
+    for(int j=1; j<=3; j++){
+      log("$i  $j");
+      if( i==2 && j==2 ){
+        break myOuterLoop;
+      }
+    }
+  }
+  //Nested For Loop
+  for(int i= 1; i<=10; i++ ){
+    for(int j=1; j<=i; j++){
+      log("$i");
+    }
+  }
+
+
 }
