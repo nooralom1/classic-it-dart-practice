715687487cadcd75df105913bf21c74f558d8b07
NS NooR <non377938@gmail.com>
2024-08-17 22:38:23 +0600

    For Loop

--- lib/note.dart
+++ lib/note.dart
@@ -124,4 +124,20 @@ void main() {
       log("Invalid Grade");
   }
 
+
+  //For Loop
+
+  for(int i= 1; i<=10; i++){
+    if(i%2==0){
+      log("$i");
+    }
+  }
+
+  //For in Loop
+  List planetList = ["Mercury", "Venus", "Earth", "Mars",];
+
+  for(String planet in planetList){
+    log(planet);
+  }
+
 }
