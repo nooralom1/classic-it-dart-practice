f32139bddb9c52c73a6ce505be9dda78c0893506
NS NooR <non377938@gmail.com>
2024-08-17 10:53:52 +0600

    if else statements

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,34 @@
-void main() {
-  int marks = 60;
+import 'dart:developer';
 
-  if(marks>=80&&marks<=100){
-    print("result is A+");
+void main() {
+  //if and else statements
+
+  var salary = 15000;
+
+  if (salary > 20000) {
+    log("you got promotion. Congratulations !");
+  } else {
+    log("you need to work hard !");
   }
-  else  if(marks>=70&&marks<=79){
-    print("result is A");
-  }
-  else  if(marks>=60&&marks<=69){
-    print("result is A-");
-  }
-  else  if(marks>=50&&marks<=59){
-    print("result is B+");
-  }
-  else  if(marks>=40&&marks<=49){
-    print("result is B");
-  }
-  else  if(marks>=33&&marks<=39){
-    print("result is C");
-  }
-  else{
-    print("F");
+
+  //if elseif else statements
+  int marks = 70;
+
+  if (marks >= 80 && marks <= 100) {
+    log("result is A+");
+  } else if (marks >= 70 && marks <= 79) {
+    log("result is A");
+  } else if (marks >= 60 && marks <= 69) {
+    log("result is A-");
+  } else if (marks >= 50 && marks <= 59) {
+    log("result is B+");
+  } else if (marks >= 40 && marks <= 49) {
+    log("result is B");
+  } else if (marks >= 33 && marks <= 39) {
+    log("result is C");
+  } else if (marks>=0 && marks<33) {
+    log("F");
+  }else{
+    log("Invalid marks please input valid marks");
   }
 }
